7f84dc4a3bdee87fe1cd1609f723baba5f199aa5
maidou <maidousuiyue@163.com>
2018-07-01 13:37:20 +0800

    [修改] swagger 描述语

--- src/main/java/com/config/Swagger2Config.java
+++ src/main/java/com/config/Swagger2Config.java
@@ -3,7 +3,6 @@ package com.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
